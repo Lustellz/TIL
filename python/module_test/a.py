8df133e383699c08560b5176e1391050857e1ad0
Ryeonggyeong Gwon <lustellz.gmail.com>
2019-01-09 17:57:16 +0900

    D06 | 190109 PM | Data structure (Dict, Set)

---
+++ python/module_test/a.py
@@ -0,0 +1,10 @@
+#1. 직접 실행
+#__name__='__main__'
+#2. import
+#__name__='a'
+
+print(__name__)
+if __name__=='__main__':
+    print('a.py를 직접 실행')
+else:
+    print('a.py가 import 되어 실행')
---
+++ python/module_test/b.py
@@ -0,0 +1,6 @@
+import a
+from my_math #import add, mul
+#import my_math
+import random
+print(my_math.add(2,3))
+print(my_math.mul(2,3))
